f56dee39ca92ce37bd1fb6e653816f98514d6817
Helppo Elämä <35959304+Nastradoomus@users.noreply.github.com>
2020-06-09 11:25:48 +0300

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,8 @@
 import 'bootstrap';
+(function (global) {
+    global.$ = $;
+    global.jQuery = jQuery;
+})(typeof window !== "undefined" ? window : this);
 
 $(document).ready(function () {
     console.log('Webpack loaded!');
